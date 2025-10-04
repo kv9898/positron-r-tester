593086967412554f5abae4ca8016e4dcc68f3960
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2025-10-04 22:07:29 +0100

    Add constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2022-2024 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as path from 'path';
+import * as fs from 'fs-extra';
+
+// The extension root directory.
+export const EXTENSION_ROOT_DIR = path.join(__dirname, '..');
+
+// Read the package.json file.
+const packageJson = fs.readJSONSync(path.join(EXTENSION_ROOT_DIR, 'package.json'));
+
+// The minimum supported version of R.
+export const MINIMUM_R_VERSION = packageJson.positron.minimumRVersion as string;
+
+// The minimum supported version of renv.
+export const MINIMUM_RENV_VERSION = packageJson.positron.minimumRenvVersion as string;
+
+// The settings key for the default R interpreter path in Positron.
+export const POSITRON_R_INTERPRETERS_DEFAULT_SETTING_KEY = 'positron.r.interpreters.default';
