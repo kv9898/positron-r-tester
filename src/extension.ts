1481c1c163c217f2ee6f313ec45aa0865edbe070
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2025-10-04 17:26:16 +0100

    init with extension.ts

---
+++ src/extension.ts
@@ -0,0 +1,38 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2023-2025 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+
+import { registerCommands } from './commands';
+import { providePackageTasks } from './tasks';
+import { setContexts } from './contexts';
+import { setupTestExplorer, refreshTestExplorer } from './testing/testing';
+
+export const LOGGER = vscode.window.createOutputChannel('R Tester', { log: true });
+
+export function activate(context: vscode.ExtensionContext) {
+	const onDidChangeLogLevel = (logLevel: vscode.LogLevel) => {
+		LOGGER.appendLine(vscode.l10n.t('Log level: {0}', vscode.LogLevel[logLevel]));
+	};
+	context.subscriptions.push(LOGGER.onDidChangeLogLevel(onDidChangeLogLevel));
+	onDidChangeLogLevel(LOGGER.logLevel);
+
+	// Set contexts.
+	setContexts(context);
+
+	// Register commands.
+	registerCommands(context);
+
+	// Provide tasks.
+	providePackageTasks(context);
+
+	// Setup testthat test explorer.
+	setupTestExplorer(context);
+	vscode.workspace.onDidChangeConfiguration(async event => {
+		if (event.affectsConfiguration('positron.r.testing')) {
+			refreshTestExplorer(context);
+		}
+	});
+}
