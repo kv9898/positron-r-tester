2edb05077328d0bc594e372fed3fbe73f8061220
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2025-10-04 21:15:36 +0100

    Add util-testing.ts

---
+++ src/testing/util-testing.ts
@@ -0,0 +1,42 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2023 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import { LOGGER } from '../extension';
+
+export enum ItemType {
+    Directory = 'directory',
+    File = 'file',
+    TestThat = 'test_that',
+    Describe = 'describe',
+    It = 'it',
+}
+
+export interface TestingTools {
+    packageRoot: vscode.Uri;
+    packageName: string;
+    controller: vscode.TestController;
+    testItemData: WeakMap<vscode.TestItem, ItemType>;
+}
+
+export function encodeNodeId(
+    testFile: string,
+    testLabel: string | undefined = undefined,
+    testSuperLabel: string | undefined = undefined
+) {
+    return testSuperLabel
+        ? `${testFile}&${testSuperLabel}: ${testLabel}`
+        : testLabel
+            ? `${testFile}&${testLabel}`
+            : testFile;
+}
+
+export interface TestParser {
+    (testingTools: TestingTools, file: vscode.TestItem): Promise<void>;
+}
+
+export interface TestRunner {
+    (testingTools: TestingTools, run: vscode.TestRun, test: vscode.TestItem): Promise<string>;
+}
