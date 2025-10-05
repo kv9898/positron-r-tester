f304e55535320d5a0f5d510aa5bdb55a0c57b88e
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2025-10-05 08:48:17 +0100

    parser: use default explicitly

--- src/testing/parser.ts
+++ src/testing/parser.ts
@@ -19,8 +19,8 @@ let _queryStrings: string[] | null = null;
 // lazily initialize the parser
 export async function initializeParser(): Promise<Parser> {
 	LOGGER.info(`Initializing parser`);
-	await Parser.init();
-	const parser = new Parser();
+	await Parser.default.init();
+	const parser = new Parser.default();
 	LOGGER.info(`tree-sitter-r.wasm path: ${wasmPath}`);
 	R = await Parser.Language.load(wasmPath);
 	parser.setLanguage(R);
